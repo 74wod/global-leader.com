0b709a802507aecbb4aba183dfce221568a3d2e6
74wod <jvyom1998@gmail.com>
2025-04-02 18:03:36 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Global Leaders Dating App</h1>
+      <p>Welcome to the world of legendary leaders!</p>
+    </div>
+  );
+}
+
+export default App;
